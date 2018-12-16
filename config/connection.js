97dba57507df85e25bd0e9cea9b17f20af24e014
rgao <xinghoshi@gmail.com>
2018-12-15 22:28:23 -0800

    included jawsdb_url addon to connection configuration

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Zubenelakrab",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-connection.connect(function(error) {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Zubenelakrab",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function (error) {
     if (error) {
         console.error("error connection: " + error.stack);
         return;
